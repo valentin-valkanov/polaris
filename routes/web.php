3b5fc688e2e73b7ed17e706cd4a904fd8c34ccf9
Valentin Valkanov <valentinvalkanof@gmail.com>
2025-01-20 12:58:36 +0200

    Setting up Laravel, creating three layouts: Dashboard, Positions, and Stats

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+Route::get('positions', function (){
+    return view('positions');
+});
+
+Route::get('stats', function (){
+    return view('stats');
 });
